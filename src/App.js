dc60b58a38dd67a1a72e4ddf17951f31a081f5a8
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2020-10-02 13:21:02 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "assets/scss/style.scss";
+import Button from "elements/Button";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+function App() {
+  return <Router></Router>;
+}
+
+export default App;
